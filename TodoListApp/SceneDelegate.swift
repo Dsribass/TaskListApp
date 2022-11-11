82551ff63748fced8d3dac513ac19b609e7445f7
Daniel Ribas <danielribas@tokenlab.com.br>
2022-11-11 13:03:51 -0300

    initial setup

---
+++ TodoListApp/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  TodoListApp
+//
+//  Created by Daniel de Souza Ribas on 11/11/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+
+    window = getAppWindow(windowScene: windowScene)
+  }
+
+  private func getAppWindow(windowScene: UIWindowScene) -> UIWindow {
+    let safeWindow = UIWindow(windowScene: windowScene)
+    safeWindow.frame = UIScreen.main.bounds
+    safeWindow.makeKeyAndVisible()
+    safeWindow.rootViewController = ViewController()
+
+    return safeWindow
+  }
+}
